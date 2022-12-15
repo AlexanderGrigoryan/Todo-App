58f6ddf076cff013c42f655415de9d12bb102076
Alexander Grigoryan <a.grigoryan@inbox.ru>
2022-12-15 19:25:39 +0400

    style: make background of header

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+import LightBackground from "../img/bg-mobile-light.jpg"
+
+function Header() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Header
+
+const Container = styled.div`
+background: url(${LightBackground});
+height: 200px;
+width: 100%;
+background-repeat: no-repeat;
+background-size: 100%;
+
+`;
+
